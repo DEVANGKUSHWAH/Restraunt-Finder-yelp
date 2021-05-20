400162537989c324bace0d8fe1611e7635b366a4
DEVANG KUSHWAH <67060334+DEVANGKUSHWAH@users.noreply.github.com>
2021-05-20 22:30:58 +0530

    Rev. Postgres T., Star Rating C., Rev. C. Layout,

--- src/routes/RestaurantDetailPage.jsx
+++ src/routes/RestaurantDetailPage.jsx
@@ -2,9 +2,9 @@ import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { RestaurantsContext } from '../context/RestaurantsContext';
 import RestaurantFinder from '../apis/RestaurantFinder';
-// import StarRating from '../components/StarRating';
-// import Reviews from '../components/Reviews';
-// import AddReview from '../components/AddReview';
+import StarRating from '../components/StarRating';
+import Reviews from '../components/Reviews';
+import AddReview from '../components/AddReview';
 
 const RestaurantDetailPage = () => {
 	const { id } = useParams();
@@ -32,20 +32,10 @@ const RestaurantDetailPage = () => {
 					<h1 className="text-center display-1">
 						{selectedRestaurant.restaurant.name}
 					</h1>
-					<div className="text-center">
-						{/* <StarRating
-							rating={selectedRestaurant.restaurant.average_rating}
-						/> */}
-						<span className="text-warning ml-1">
-							{selectedRestaurant.restaurant.count
-								? `(${selectedRestaurant.restaurant.count})`
-								: '(0)'}
-						</span>
-					</div>
-					{/* <div className="mt-3">
+					<div className="mt-3">
 						<Reviews reviews={selectedRestaurant.reviews} />
-					</div> */}
-					{/* <AddReview /> */}
+					</div>
+					<AddReview />
 				</>
 			)}
 		</div>
